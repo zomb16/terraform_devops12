2a6c7e1e0ab4257532b2735e0acedf8b26d7f5fb
Zomb <100679615+zomb16@users.noreply.github.com>
2023-01-19 15:36:03 +1100

    Delete main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
-  subscription_id = "aaaaaaaaaaaaa"
-  client_id       = "bbbbbbbbbbbbbba"
-  client_secret   = "cccccccccccccccc"
-  tenant_id       = "ddddddddddddddddd"
-  features {}
-}
-
