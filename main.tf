dbdfbc862d0293ad4b7c3364a7bcb181bc92a9d7
Zomb <100679615+zomb16@users.noreply.github.com>
2023-01-19 14:47:46 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  subscription_id = "eacda16f-01ab-4e1f-a254-800bbe7ab869"
-  client_id       = "5174db6a-39e9-4000-a6e4-b2a2da6f74aa"
-  client_secret   = "Co68Q~Wk2dtl0dANk.QUj0xGS44w8akBNZfAadnW"
-  tenant_id       = "6a2fb94e-5b0a-4e0d-9bcd-d99ff610a37d"
+  subscription_id = "aaaaaaaaaaaaa"
+  client_id       = "bbbbbbbbbbbbbba"
+  client_secret   = "cccccccccccccccc"
+  tenant_id       = "ddddddddddddddddd"
   features {}
 }
 
